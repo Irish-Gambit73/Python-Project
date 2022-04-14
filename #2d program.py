4e6c2151eadaa1945366bff7714dd0704de44f1f
Irish-Gambit73 <80124804+Irish-Gambit73@users.noreply.github.com>
2022-04-14 14:41:30 -0700

    Update #2d program.py

--- #2d program.py
+++ #2d program.py
@@ -1,6 +1,7 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFileData
 from direct.task import Task, TaskManagerGlobal
+import simplepbr
 
 configVars = """
 win-size 1280 720
@@ -9,47 +10,59 @@ show-frame-rate-meter 1
 loadPrcFileData("", configVars)
 
 # Window Display
+
+
 class ShowBase(ShowBase):
     def __init__(self):
         super().__init__()
 
+        simplepbr.init()
         self.floor = self.loader.loadModel("Models/floor")
         self.floor.reparentTo(self.render)
 
-        # just for now
-        self.set_background_color(0, 0, 1, 0)
+        self.Guts = self.loader.loadModel("Models/Guts.glb")
+        self.Guts.reparentTo(self.render)
+        self.Guts.setScale(0.05)
+        self.Guts.setPos(0, 0, 1)
 
+        self.set_background_color(0, 0, 1, 0) 
+    def start(self):
 
-# For Player(s)
-class Player():
-    def __init__(self):
+        self.keyMap = {"left": 0, "right": 0, "forward": 0, "backward": 0}
 
-        # Loading Models
-        self.player = self.loader.loadModel("Models/player")
-        self.player.reparentTo(self.render)
-        self.player = Player(0, 1)
-        self.player.show()
-        # Constants
-        self.Speed = 4.0
-        self.Weight = -0.05
-        self.Vertical = 1.2
-        self.Friction = -0.12
-        # Button Associations
-        self.leftButton = KeyboardButton.asciiKey(b"a")
-        self.rightButton = KeyboardButton.asciiKey(b"d")
+        self.accept("escape", exit)
+        self.accept("a", self.setKey, ["left", 1])
+        self.accept("d", self.setKey, ["right", 1])
+        self.accept("w", self.setKey, ["forward", 1])
+        self.accept("s", self.setKey, ["backward", 1])
+        self.accept("a-up", self.setKey, ["left", 0])
+        self.accept("d-up", self.setKey, ["right", 0])
+        self.accept("w-up", self.setKey, ["forward", 0])
+        self.accept("s-up", self.setKey, ["backward", 0])
 
-        def moveTask(self, task):
-            speed = 0.0
-            isDown = base.mouseWatcherNode.isButtonDown
-            if isDown(self.leftButton):
-                speed += self.walkSpeed
-            if isDown(self.rightButton):
-                speed -= self.walkSpeed
-            yDelta = speed * globalClock.get
-            self.character.setY(self.character, yDelta)
-            return task.cont 
+        taskMgr.add(self.move, "task_movement", priority=-10)
+        self.request("Idle")
+    def setKey(self, key, value):
+        self.keyMap[key] = value
 
-            #FSM animations
+    def move(self, task):
+        dt = globalClock.getDt()
+        requestState = "Idle"
+        if self.keyMap["left"] != 0:
+            self.player.setH(self.player.getH() + 150 * dt)
+            requestState = "Walk"
+        if self.keyMap["right"] != 0:
+            self.player.setH(self.player.getH() - 150 * dt)
+            requestState = "Walk"
+        if self.keyMap["forward"] != 0:
+            self.player.setY(self.player, -15 * dt)
+            requestState = "Walk"
+        if self.keyMap["backward"] != 0:
+            self.player.setY(self.player, 15 * dt)
+            requestState = "Walk"
+        if self.state != requestState:
+            self.request(requestState)
+        return task.cont
 
 
 base = ShowBase()
