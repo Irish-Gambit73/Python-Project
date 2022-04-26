792f698339c1e41f4431a64afaeb3ab67332489d
Irish-Gambit73 <80124804+Irish-Gambit73@users.noreply.github.com>
2022-04-26 14:37:14 -0700

    Update #2d program.py

--- #2d program.py
+++ #2d program.py
@@ -1,7 +1,9 @@
+from turtle import isdown
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import loadPrcFileData
+from panda3d.core import loadPrcFileData, KeyboardButton, AmbientLight, DirectionalLight
 from direct.task import Task, TaskManagerGlobal
 import simplepbr
+import Player.py
 
 configVars = """
 win-size 1280 720
@@ -12,58 +14,35 @@ loadPrcFileData("", configVars)
 # Window Display
 
 
-class ShowBase(ShowBase):
+class game(ShowBase):
     def __init__(self):
-        super().__init__()
+        ShowBase.__init__(self)
 
-        simplepbr.init()
-        self.floor = self.loader.loadModel("Models/floor")
-        self.floor.reparentTo(self.render)
+        ambientLight = AmbientLight("ambientLight")
+        ambientLight.setColor((.3, .3, .3, 1))
+        directionalLight = DirectionalLight("directionalLight")
+        directionalLight.setDirection((-5, -5, -5))
+        directionalLight.setColor((1, 1, 1, 1))
+        directionalLight.setSpecularColor((1, 1, 1, 1))
+        render.setLight(render.attachNewNode(ambientLight))
+        render.setLight(render.attachNewNode(directionalLight))
 
-        self.Guts = self.loader.loadModel("Models/Guts.glb")
-        self.Guts.reparentTo(self.render)
-        self.Guts.setScale(0.05)
-        self.Guts.setPos(0, 0, 1)
-
-        self.set_background_color(0, 0, 1, 0) 
-    def start(self):
-
-        self.keyMap = {"left": 0, "right": 0, "forward": 0, "backward": 0}
-
-        self.accept("escape", exit)
-        self.accept("a", self.setKey, ["left", 1])
-        self.accept("d", self.setKey, ["right", 1])
-        self.accept("w", self.setKey, ["forward", 1])
-        self.accept("s", self.setKey, ["backward", 1])
-        self.accept("a-up", self.setKey, ["left", 0])
-        self.accept("d-up", self.setKey, ["right", 0])
-        self.accept("w-up", self.setKey, ["forward", 0])
-        self.accept("s-up", self.setKey, ["backward", 0])
-
-        taskMgr.add(self.move, "task_movement", priority=-10)
-        self.request("Idle")
-    def setKey(self, key, value):
-        self.keyMap[key] = value
-
-    def move(self, task):
-        dt = globalClock.getDt()
-        requestState = "Idle"
-        if self.keyMap["left"] != 0:
-            self.player.setH(self.player.getH() + 150 * dt)
-            requestState = "Walk"
-        if self.keyMap["right"] != 0:
-            self.player.setH(self.player.getH() - 150 * dt)
-            requestState = "Walk"
-        if self.keyMap["forward"] != 0:
-            self.player.setY(self.player, -15 * dt)
-            requestState = "Walk"
-        if self.keyMap["backward"] != 0:
-            self.player.setY(self.player, 15 * dt)
-            requestState = "Walk"
-        if self.state != requestState:
-            self.request(requestState)
-        return task.cont
+        
 
 
-base = ShowBase()
-base.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+game = ShowBase()
+game.run()
