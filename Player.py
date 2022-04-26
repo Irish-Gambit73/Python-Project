9706dd5a3b61c7a1848c5af1ad5b81859451f7bd
Irish-Gambit73 <80124804+Irish-Gambit73@users.noreply.github.com>
2022-04-26 14:37:37 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,22 +1,25 @@
 #player
 
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import KeyboardButton
+from panda3d.core import KeyboardButton, Quat, Vec3
+from panda3d.core import CollisionHandlerQueue, CollisionNode, CollisionSphere, CollisionTraverser, BitMask32
 import sys, simplepbr
-
+import direct.directbase.DirectStart
 
 class Player(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-
+        
         self.keyMap = {
             "left": 0, "right": 0, "forward": 0, "backward": 0, "leftp2": 0, "rightp2": 0, "forwardp2": 0, "backwardp2": 0}
 
         simplepbr.init()
         self.Guts = self.loader.loadModel("Models/Guts.glb")
         self.Guts.reparentTo(self.render)
-        self.Guts.setScale(0.05)
-        self.Guts.setPos(0, 5, 1)
+        self.Guts.setScale(0.001)
+        self.Guts.setPos(-0.5, 3, -0.30)
+        self.Guts.setGravity(0, 0, -9.81)
+        
 
         self.accept("escape", sys.exit)
         self.accept("a", self.setKey, ["left", True])
@@ -30,11 +33,13 @@ class Player(ShowBase):
 
         self.floor = self.loader.loadModel("Models/floor")
         self.floor.reparentTo(self.render)
+        self.floor.setScale(0.05) 
+        self.floor.setPos(0, 3, -0.30)
 
         self.casca = self.loader.loadModel("Models/player.egg")
         self.casca.reparentTo(self.render)
         self.casca.setScale(0.05)
-        self.casca.setPos(0, 5, 1)
+        self.casca.setPos(-0.05, 3, -0.3)
 
         self.accept("j", self.setKey, ["leftp2", True])
         self.accept("l", self.setKey, ["rightp2", True])
@@ -45,9 +50,7 @@ class Player(ShowBase):
         self.accept("k", self.setKey, ["backwardp2", True])
         self.accept("k-up", self.setKey, ["backwardp2", False])
 
-
         taskMgr.add(self.move, "moveTask")
-
         self.isMoving = False
     def setKey(self, key, value):
         self.keyMap[key] = value
@@ -58,22 +61,19 @@ class Player(ShowBase):
         if self.keyMap["right"]:
             self.Guts.setH(self.Guts.getH() - 300 * dt)
         if self.keyMap["forward"]:
-            self.Guts.setY(self.Guts, -25 * dt)
+            self.Guts.setY(self.Guts, -50 * dt)
         if self.keyMap["backward"]:
-            self.Guts.setY(self.Guts, 25 * dt)
+            self.Guts.setY(self.Guts, 30 * dt)
         
         if self.keyMap["leftp2"]:
             self.casca.setH(self.casca.getH() + 300 * dt)
         if self.keyMap["rightp2"]:
             self.casca.setH(self.casca.getH() - 300 * dt)
         if self.keyMap["forwardp2"]:
-            self.casca.setY(self.casca, -25 * dt)
+            self.casca.setY(self.casca, -50 * dt)
         if self.keyMap["backwardp2"]:
-            self.casca.setY(self.casca, 25 * dt)
+            self.casca.setY(self.casca, 30 * dt)
         
         return task.cont
-
-
-
 PlayerScript = Player()
 PlayerScript.run()
