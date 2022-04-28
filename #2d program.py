b6d31d17ac17a560d4797f57e8ad8dc55ee04d8b
Irish-Gambit73 <80124804+Irish-Gambit73@users.noreply.github.com>
2022-04-28 14:43:25 -0700

    Update #2d program.py

--- #2d program.py
+++ #2d program.py
@@ -1,22 +1,20 @@
 from turtle import isdown
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import loadPrcFileData, KeyboardButton, AmbientLight, DirectionalLight
+from panda3d.core import loadPrcFileData, AmbientLight, DirectionalLight
 from direct.task import Task, TaskManagerGlobal
 import simplepbr
 import Player.py
-
-configVars = """
-win-size 1280 720
-show-frame-rate-meter 1
-"""
-loadPrcFileData("", configVars)
-
-# Window Display
 
 
 class game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
+        configVars = """
+        win-size 1280 720
+        show-frame-rate-meter 1
+        """
+        loadPrcFileData("", configVars)
 
         ambientLight = AmbientLight("ambientLight")
         ambientLight.setColor((.3, .3, .3, 1))
@@ -26,6 +24,10 @@ class game(ShowBase):
         directionalLight.setSpecularColor((1, 1, 1, 1))
         render.setLight(render.attachNewNode(ambientLight))
         render.setLight(render.attachNewNode(directionalLight))
+
+
+
+    
 
         
 
