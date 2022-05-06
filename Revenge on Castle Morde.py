efebda220107dab3af273104c1a2a5e26f400dcc
Irish-Gambit73 <80124804+Irish-Gambit73@users.noreply.github.com>
2022-05-06 14:44:06 -0700

    Add files via upload

---
+++ Revenge on Castle Morde.py
@@ -0,0 +1,140 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import loadPrcFileData, Vec3
+from panda3d.core import CollisionBox, CollisionTraverser, CollisionHandlerQueue, CollisionNode, BitMask32
+
+configVars = """
+win-size 1280 720
+show-frame-rate-meter 1
+"""
+
+loadPrcFileData("", configVars)
+
+key_map = {
+    "left": False,
+    "right": False,
+    "left2": False,
+    "right2": False,
+}
+
+
+def update_key_map(control_name, state):
+    """ This function is called when the left or right keys are pressed or released. It updates the key_map dict."""
+    key_map[control_name] = state
+
+
+class Platformer(ShowBase):
+    def __init__(self):
+        super().__init__()
+        self.set_background_color(0, 0, 0, 1)
+        self.cam.setPos(0, -65, 15)
+
+        # Guts
+        self.player = self.loader.loadModel("Models/Guts.glb")
+        self.player.node().setIntoCollideMask(BitMask32.bit(2))
+        self.player.reparentTo(self.render)
+        self.player.setScale(1)
+        self.player.setHpr(90, 0, 0)
+
+        # Flor
+        self.floor = self.loader.loadModel("Models/floor")
+        self.floor.reparentTo(self.render)
+
+        # Godo
+        self.player2 = self.loader.loadModel("Models/godo.glb")
+        self.player2.reparentTo(self.render)
+        self.player2.setScale(1)
+
+
+        # keyboard input
+        self.accept("a", update_key_map, ["left", True])
+        self.accept("a-up", update_key_map, ["left", False])
+        self.accept("d", update_key_map, ["right", True])
+        self.accept("d-up", update_key_map, ["right", False])
+        self.accept("w", self.jump)
+        self.accept("l", update_key_map, ["left2", True])
+        self.accept("l-up", update_key_map, ["left2", False])
+        self.accept("'", update_key_map, ["right2", True])
+        self.accept("'-up", update_key_map, ["right2", False])
+        self.accept("p"), self.jump
+        
+
+
+        # adding the update method to the Task manager
+        self.taskMgr.add(self.update, "update")
+
+        # Movement vec's
+        self.position = Vec3(0, 0, 30)
+        self.velocity = Vec3(0, 0, 0)
+        self.acceleration = Vec3(0, 0, 0)
+
+        # Constants
+        self.SPEED = 4
+        self.GRAVITY = -0.05
+        self.JUMP_FORCE = 1.2
+        self.FRICTION = -0.12
+
+        # setting up the collision detection
+        self.cTrav = CollisionTraverser()
+        self.queue = CollisionHandlerQueue()
+        collider_node = CollisionNode("box-coll")  # collision node for the Player
+        coll_box = CollisionBox((-1, -1, 0), (1, 1, 4))  # collision geometry for the Player
+        collider_node.setFromCollideMask(BitMask32.bit(1))
+        collider_node.addSolid(coll_box)
+        collider = self.player.attachNewNode(collider_node)
+        self.cTrav.addCollider(collider, self.queue)
+        collider.show()
+
+        # Jump variables
+        self.is_jumping = False
+        self.is_on_floor = True
+        self.jump_count = 0
+
+    def jump(self):
+        if self.is_on_floor:
+            self.is_jumping = True
+            self.is_on_floor = False
+            self.velocity.z = self.JUMP_FORCE
+            self.jump_count += 1
+            if self.jump_count == 2:
+                self.is_jumping = False
+                self.is_on_floor = True
+                self.jump_count = 0
+
+    def update(self, task):
+        dt = globalClock.getDt()
+
+        self.acceleration = Vec3(0, 0, self.GRAVITY)
+
+        if key_map["right"]:  # if right is True
+            self.acceleration.x = self.SPEED * dt
+        if key_map["left"]:  # if left is True
+            self.acceleration.x = -self.SPEED * dt
+        if key_map["right2"]:
+            self.acceleration.x = self.SPEED * dt
+        if key_map["left2"]:
+            self.accelration.x = self.SPEED * dt
+
+        # calculating the position vector based on the velocity and the acceleration vectors
+        self.acceleration.x += self.velocity.x * self.FRICTION
+        self.velocity += self.acceleration
+        self.position += self.velocity + (self.acceleration * 0.5)
+
+        for entry in self.queue.getEntries():
+
+            inp = entry.getIntoNodePath().getPos(self.render)
+
+            if self.velocity.z < 0:  # prevent snapping to the top of the platforms
+                if not self.is_jumping:
+                    self.position.z = inp.z
+                    self.velocity.z = 0  # prevent fast falling from platforms
+                    self.is_on_floor = True
+                else:
+                    self.is_jumping = False
+
+        self.player.setPos(self.position)
+
+        return task.cont
+
+
+game = Platformer()
+game.run()
