1bc4ae76c546fbb66325c6e6ead5964d68402de3
Irish-Gambit73 <80124804+Irish-Gambit73@users.noreply.github.com>
2022-06-08 14:44:18 -0700

    Update Revenge on Castle Morde.py

--- Revenge on Castle Morde.py
+++ Revenge on Castle Morde.py
@@ -1,9 +1,10 @@
-
+from multiprocessing.connection import wait
 from turtle import delay, update
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFileData, Vec3
-from panda3d.core import CollisionBox, CollisionTraverser, CollisionHandlerQueue, CollisionNode, BitMask32, WindowProperties, ConfigPageManager, ConfigVariableInt, ConfigVariableBool, ConfigVariableString, AntialiasAttrib, CollisionHandlerPusher, CollisionSegment
+from panda3d.core import CollisionBox, CollisionTraverser, CollisionHandlerQueue, CollisionHandlerEvent, CollisionNode, BitMask32, WindowProperties, ConfigPageManager, ConfigVariableInt, ConfigVariableBool, ConfigVariableString, AntialiasAttrib, CollisionHandlerPusher, CollisionSegment
 from panda3d.core import *
+from direct.interval.IntervalGlobal import Sequence, Func, Wait
 from direct.task import Task
 from time import time
 import time, os, math, sys
@@ -27,7 +28,6 @@ key_map = {
 def update_key_map(control_name, state):
     key_map[control_name] = state
 
-
 class RevengeOnCastleMorde(ShowBase):
     def __init__(self):
         super().__init__()
@@ -37,6 +37,8 @@ class RevengeOnCastleMorde(ShowBase):
         self.camLens.setNear(0.8)
         self.render.setAntialias(AntialiasAttrib.MAuto)
         render.setShaderAuto()
+
+        round = 0
 
         particles = ConfigVariableBool("particles-enabled", True).getValue()
         if particles:
@@ -51,6 +53,7 @@ class RevengeOnCastleMorde(ShowBase):
         self.player.setHpr(90, 0, 0)
         self.playerhealth = 100
         self.alive = True
+
         # Flor
         self.floor = self.loader.loadModel("Models/floor")
         self.floor.reparentTo(self.render)
@@ -69,12 +72,22 @@ class RevengeOnCastleMorde(ShowBase):
         self.enemy1.reparentTo(self.render)
         self.enemy1.setScale(1)
         self.enemy1.setHpr(-90, 0, 0)
-        self.enemy1health == 50
+        self.enemy1health = 50
         self.enemyisalive = True
         enemy1pos = Vec3(-10, 0, 0)
         self.enemy1.setPos(enemy1pos)
         self.enemy1attackdistance = 1.5
 
+        # Enemy2
+        self.enemy2 = self.loader.loadModel("Models/brick_wall.gltf")
+        self.enemy2.reparentTo(self.render)
+        self.enemy2.setScale(1)
+        self.enemy2.setHpr(-90, 0, 0)
+        self.enemy2health = 50
+        self.enemy2isalive = True
+        enemy2pos = Vec3(10, 0, 0)
+        self.enemy2.setPos(enemy2pos)
+        self.enemy2attackdistance = 1.5
 
         # keyboard input
         self.accept("a", update_key_map, ["left", True])
@@ -99,8 +112,8 @@ class RevengeOnCastleMorde(ShowBase):
         # Lighting
         light = Spotlight('light')
         light_np = self.render.attachNewNode(light)
-        light_np.set_pos(50, 50, 25)
-        light_np.look_at(0, 1, 0)
+        light_np.set_pos(0, 0, 75)
+        light_np.look_at(0, -1, 0)
         light.setShadowCaster(True)
         light.getLens().setNearFar(1, 100)
         self.render.setLight(light_np)
@@ -108,12 +121,14 @@ class RevengeOnCastleMorde(ShowBase):
         plight = PointLight("plight")
         plight.setColor((1, 1, 1, 1))
         plnp = render.attachNewNode(plight)
-        plnp.setPos(0, 65, 15)
+        plnp.look_at(0, 0, -1)
+        plnp.setPos(0, 0, 6)
         render.setLight(plnp)
 
         alight = AmbientLight("alight")
         alight.setColor((0.08, 0.08, 0.08, 1))
         alnp = render.attachNewNode(alight)
+        alnp.set_pos(0, 0, 3)
         render.setLight(alnp)
 
         # Movement vec's for player1 and player2
@@ -141,7 +156,14 @@ class RevengeOnCastleMorde(ShowBase):
         self.JUMP_FORCE2 = 1.1
         self.FRICTION2 = -0.14
 
-        # Collision detection
+        #Collisions                                  ----------------------------------------------------------------------------
+        self.collCount = 0
+
+        self.collHandEvent = CollisionHandlerEvent()
+        self.collHandEvent.addInPattern('into-%in')
+        self.collHandEvent.addOutPattern('outof-%in')
+
+        # P1 Collision
         self.cTrav = CollisionTraverser()
         self.queue = CollisionHandlerQueue()
         collider_node = CollisionNode("p1coll")
@@ -151,7 +173,7 @@ class RevengeOnCastleMorde(ShowBase):
         collider = self.player.attachNewNode(collider_node)
         self.cTrav.addCollider(collider, self.queue)
         
-        # Attack Ray
+        # P1 Attack Coll. Ray
         self.queue4 = CollisionHandlerQueue()
         attackray = CollisionSegment(0, -6.8, 5, -5, 0, 5)
         attackray_node = CollisionNode("attackcol")
@@ -161,6 +183,7 @@ class RevengeOnCastleMorde(ShowBase):
         self.cTrav.addCollider(attackraycollider, self.queue4)
         attackraycollider.show()
 
+        # P2 Collision
         self.queue2 = CollisionHandlerQueue()
         collider_nodep2 = CollisionNode("p2coll")
         coll_box2 = CollisionBox((-1, -1, 0), (1, 1, 6))
@@ -169,6 +192,8 @@ class RevengeOnCastleMorde(ShowBase):
         collider2 = self.player2.attachNewNode(collider_nodep2)
         self.cTrav.addCollider(collider2, self.queue2)
 
+
+        # P2 Attack Coll. ray
         self.queue5 = CollisionHandlerQueue()
         attackray2 = CollisionSegment(0, -6.8, 5, -5, 0, 5)
         attackray_node2 = CollisionNode("attackcol2")
@@ -178,20 +203,33 @@ class RevengeOnCastleMorde(ShowBase):
         self.cTrav.addCollider(attackraycollider2, self.queue5)
         attackraycollider2.show()
 
-        self.queue3 = CollisionHandlerQueue()
+        # Enemy1 collision
+        self.queue101 = CollisionHandlerQueue()
         collider_nodee1 = CollisionNode("e1coll")
-        coll_boxe1 = CollisionBox((-1.5, -1.5, 0), (1, 2, 8))
+        coll_boxe1 = CollisionBox((-3.7, -3, 0), (3.7, 3, 8))
         collider_nodee1.setFromCollideMask(BitMask32.bit(1))
         collider_nodee1.addSolid(coll_boxe1)
         collidere1 = self.enemy1.attachNewNode(collider_nodee1)
-        self.cTrav.addCollider(collidere1, self.queue3)
+        self.cTrav.addCollider(collidere1, self.queue101)
+
+        #Enemy2 Collision
+        self.queue102 = CollisionHandlerQueue()
+        collider_nodee2 = CollisionNode("e2coll")
+        coll_boxe2 = CollisionBox((-3.7, -3, 0), (3.7, 3, 8))
+        collider_nodee2.setFromCollideMask(BitMask32.bit(1))
+        collider_nodee2.addSolid(coll_boxe2)
+        collidere2 = self.enemy2.attachNewNode(collider_nodee2)
+        self.cTrav.addCollider(collidere2, self.queue102)
+
+
+
+
+        #                                          -------------------------------------------------------
 
         if self.playerhealth < 0:
             self.gamelose
         if self.player2health < 0:
             self.gamelose
-        if self.enemy1health < 0:
-            self.enemydeath
 
 
         # Jump variables
@@ -211,14 +249,37 @@ class RevengeOnCastleMorde(ShowBase):
         self.is_not_attacking = True
         self.attackcombo = 0.0
 
+    def bumpInto(self):
+        print("Player1hasbumpedinto")
+
+    def exitOutTo(self):
+        print("Player1hasnaybequeathbumpingintoeth")
+
+    def bumpInto2(self):
+        print("Player2hasbumpedintosomething")
+
+    def exitOutTo(self):
+        print("Player2hasnaybequeathbumpingintoeth")
+
+    #If player damage > 1000 ie.   , then skip to next round
+    def beginround(self):
+        round = 1
+        if round == 1:
+            self.enemy1
+
+
 
     def gamelose(self):
         self.mainmenulaunch
+        #play a "YOU DIED" then close the application and reopen the menu
 
     def mainmenulaunch(self):
-        #play a "YOU DIED" then close the application and reopen the menu
         sys.exit
         menu.run()
+
+    def enemydeath(self):
+        #afterdeath, move it back to the spawner for reuse
+        del enemy1
 
     def jump(self):
         if self.is_on_floor:
@@ -245,6 +306,9 @@ class RevengeOnCastleMorde(ShowBase):
     def taketime(task):
         self.delayTime +=1
         return task.cont
+
+    def enemyhit(self):
+        print("HITSUCC")
 
     def debugme(self):
         self.cTrav.showCollisions(self.render)
@@ -275,7 +339,10 @@ class RevengeOnCastleMorde(ShowBase):
             #slashing
             if self.queue4.getEntries():
                 print("yahoo")
-                self.enemy1health - 10
+                self.enemy1health = (self.enemy1health - 10)
+                if self.enemy1health < 0:
+                    print("E1DEATH")
+                    self.enemydeath
 
         # Calculating the position vector based on the velocity and the acceleration vectors
         self.acceleration.x += self.velocity.x * self.FRICTION
@@ -317,10 +384,6 @@ class RevengeOnCastleMorde(ShowBase):
         self.cam.setX(self.position.x)
 
         return task.cont
-
-
-    def enemydeath(self):
-        print("me ded")
 
 game = RevengeOnCastleMorde()
 game.run()
